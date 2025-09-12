6878b309a9e21f386f938b8942cd4277904f025d
Luis Torres2 <luis_torres2@epam.com>
2025-09-12 13:44:11 -0700

    Adding acr in terraform

--- task08/modules/acr/main.tf
+++ task08/modules/acr/main.tf
@@ -1,6 +1,27 @@
 resource "azurerm_container_registry" "this" {
-  name                = ""
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku                 = "Basic"
+  name                = var.acr_name
+  resource_group_name = var.acr_resourse_group
+  location            = var.acr_location
+  sku                 = var.acr_SKU
 }
+
+resource "azurerm_container_registry_task" "this" {
+  name = var.acr_task_name
+  container_registry_id = azurerm_container_registry.this.id
+  platform {
+    os = "Linux"
+  }
+  docker_step {
+    dockerfile_path      = "Dockerfile"
+    context_path         = "https://github.com/LuisOctavio1/terraform_aks_task.git#master:task08/application"
+    context_access_token = var.git_pat
+    image_names          = [var.image_name + ":latest"]
+  }
+}
+
+resource "azurerm_container_registry_task_schedule_run_now" "this" {
+  container_registry_task_id = azurerm_container_registry_task.this.id
+}
+
+
+
--- task08/modules/acr/variables.tf
+++ task08/modules/acr/variables.tf
@@ -3,6 +3,11 @@ variable "acr_name" {
     type = string
 }
 
+variable "acr_resourse_group" {
+  description = "resource group of the acr"
+  type = string
+}
+
 variable "acr_location" {
   description = "Acr location"
   type = string
@@ -16,5 +21,17 @@ variable "acr_SKU" {
 variable "image_name" {
     description = "Name of the docker image"
     type = string
+}
+
+variable "acr_task_name" {
+  description = "name of the acr task"
+  type = string
+}
+
+
+variable "git_pat" {
+  description = "github token"
+  type = string
+  sensitive = true  
 }
 
