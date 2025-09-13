3d482d08ff1b9b3019bcfdd0da96675b9ccdf2c0
LuisOctavio1 <79322338+LuisOctavio1@users.noreply.github.com>
2025-09-13 14:35:24 -0700

    Update locals.tf

--- task08/locals.tf
+++ task08/locals.tf
@@ -1,7 +1,7 @@
 locals {
   rg_name      = format("%s-rg", var.name_prefix)
   redis_name   = format("%s-redis", var.name_prefix)
-  kv_name      = format("%s-kv", var.name_prefix)
+  keyvault_name      = format("%s-kv", var.name_prefix)
   acr_base_raw = replace(var.name_prefix, "-", "")
   acr_name     = substr("${local.acr_base_raw}acr", 0, 50)
   aci_name     = format("%s-ci", var.name_prefix)
