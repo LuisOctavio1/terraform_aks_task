eac0dc29b724bf92592cb3d0b96739d34a1ca591
Luis Octavio <luisoctavio1433@gmail.com>
2025-09-13 20:52:00 -0700

    Changing acr name

--- task08/locals.tf
+++ task08/locals.tf
@@ -3,7 +3,7 @@ locals {
   redis_name    = format("%s-redis", var.name_prefix)
   keyvault_name = format("%s-kv", var.name_prefix)
   acr_base_raw  = replace(var.name_prefix, "-", "")
-  acr_name      = substr("${local.acr_base_raw}acr", 0, 50)
+  acr_name      = substr("${local.acr_base_raw}cr", 0, 50)
   aci_name      = format("%s-ci", var.name_prefix)
   aks_name      = format("%s-aks", var.name_prefix)
 }
